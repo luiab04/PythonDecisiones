cec50c1c3e78ea8eb0a0e2a2a5b0bded852e4426
Luisa Argüelles <89225420+luiab04@users.noreply.github.com>
2022-08-23 11:52:05 -0500

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,7 +1,17 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-        
+    numero = int(input('Dame un número: '))
+def identificador(numero):
+    if numero == 0:
+        return 'Es cero'
+    if numero < 0:
+        return 'Es negativo'
+    if numero > 0:
+        return 'Es positivo'
+
+mensaje = identificador(numero)
+print(mensaje)
 
 if __name__ == '__main__':
     main()
