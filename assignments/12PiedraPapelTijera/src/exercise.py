70fe6c40c77ec4cd2c7c1557f85ede98f07f329a
Luisa Argüelles <89225420+luiab04@users.noreply.github.com>
2022-08-24 10:44:31 -0500

    Update exercise.py

--- assignments/12PiedraPapelTijera/src/exercise.py
+++ assignments/12PiedraPapelTijera/src/exercise.py
@@ -1,6 +1,26 @@
 def main():
     # Escribe tu código abajo de esta línea
-    
+   ana = input("Tirada de Ana: a) Piedra  p) Papel  t) Tijera : ")
+   juan = input("Tirada de Juan:  a) Piedra  p) Papel  t) Tijera: ")
+
+if(len(ana) > 1) or (len(juan) > 1):
+    print("Las tiradas deben ser un caracter")
+elif(ana == "a" and juan == "a"):
+    print("Empate")
+elif(juan == "p" and ana == "a"):
+    print("Gana Juan")
+elif(juan == "t" and ana == "p"):
+    print("Gana Juan")
+elif(juan == "a" and ana == "t"):
+    print("Gana Juan")
+elif(ana == "p" and juan == "a"):
+    print("Gana Ana")
+elif(ana == "t" and juan == "p"):
+    print("Gana Ana")
+elif(ana == "a" and juan == "t"):
+    print("Gana Ana")
+else:
+    print("Tirada invalida")
 
 if __name__ == '__main__':
     main()
